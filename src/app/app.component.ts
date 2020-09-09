6d4bbadafb667c26cfe8870b89b69b8e87d0998f
mahomudgamalfcis <mahmoudgamalfcis55@gmail.com>
2020-09-09 15:28:06 +0200

    refactor some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,7 @@ export class AppComponent {
   toggleClick(){
     
     this.opened=!this.opened;
-    if (this.opened==true){
-      document.getElementById('btn-navbar').style.display = "none";
-      document.getElementById('text-navbar').style.display = "none";
-    }
-    if(this.opened==false){
-      document.getElementById('btn-navbar').style.display = "block";
-      document.getElementById('text-navbar').style.display = "block";
-    }
+    
   }
 
 

a0321e7f4d35a34d906f1afebe8383e32301fa0a
mahomudgamalfcis <mahmoudgamalfcis55@gmail.com>
2021-04-04 19:07:29 +0200

    make the connection secure use https instead of http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey:'50caca6f4c4d9b12838a9e7e2431be11',
-  apiUrl:'http://api.openweathermap.org/data/2.5'
+  apiUrl:'https://api.openweathermap.org/data/2.5'
 };
